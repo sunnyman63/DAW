8a552832f841d6fbe1e71b4863d08050df6b6a9d
Alejandro Romero Pino <alerompin2@alu.edu.gva.es>
2021-12-20 20:48:06 +0100

    JS 20/12/21

--- cliente/tema5/DiezUsuariosQueCambian/js/main.js
+++ cliente/tema5/DiezUsuariosQueCambian/js/main.js
@@ -23,11 +23,39 @@ async function pedirUser(config) {
     }
 }
 
-async function ponerNUsers(users,loading) {
-    for(let i = 0;i<users.length-1;i++) {
+async function ponerNUsers(users,loading,muchos) {
+    let long = 1;
+    if(muchos) {
+        long = users.length-1;
+    } else {
+        loading.style.visibility = "visible";
+        users.firstElementChild.style.visibility = "hidden";
+    }
+
+    for(let i = 0;i<long;i++) {
 
         const user = await pedirUser(conf);
-        let data = users[i].firstElementChild.children;
+
+        let data;
+
+        if(muchos) {
+
+            if(i%2 != 0) {
+                users[i].style.backgroundColor = "#fff";
+            }
+
+            data = users[i].firstElementChild.children;
+            let boton = users[i].children[1];
+
+            boton.addEventListener("click",function(e) {
+                let data = e.target.parentNode;
+                console.log(data.lastElementChild);
+                ponerNUsers(data,data.lastElementChild,false);
+            });
+
+        } else {
+            data = users.firstElementChild.children;
+        }
 
         data[0].setAttribute("src",user.picture.large);
 
@@ -46,42 +74,41 @@ async function ponerNUsers(users,loading) {
                             +user.location.state.toUpperCase()
                             +" "
                             +user.location.country.toUpperCase()
-                            +")";
+                            +")"; 
+        if(!muchos) { users.firstElementChild.style.visibility = "visible";}
     }
     loading.style.visibility = "hidden";
 }
 
-async function cambiarUser(data) {
-    const user = await pedirUser(conf);
-    data[0].setAttribute("src",user.picture.large);
+// async function cambiarUser(data) {
+//     const user = await pedirUser(conf);
+//     data[0].setAttribute("src",user.picture.large);
 
-    data[1].innerText = user.name.first
-                        +" "
-                        +user.name.last;
+//     data[1].innerText = user.name.first
+//                         +" "
+//                         +user.name.last;
 
-    data[2].innerText = "Email: " + user.email;
+//     data[2].innerText = "Email: " + user.email;
 
-    data[3].innerText = user.location.street.number
-                        +" "
-                        +user.location.street.name;
+//     data[3].innerText = user.location.street.number
+//                         +" "
+//                         +user.location.street.name;
 
-    data[4].innerText = user.location.city
-                        +" ("
-                        +user.location.state.toUpperCase()
-                        +" "
-                        +user.location.country.toUpperCase()
-                        +")";
-}
+//     data[4].innerText = user.location.city
+//                         +" ("
+//                         +user.location.state.toUpperCase()
+//                         +" "
+//                         +user.location.country.toUpperCase()
+//                         +")";
+// }
 
 window.addEventListener("load",()=>{
 
     var users = document.body.children;
     var loading = users[10];//Es el div de loading
     //console.log(users[10]);
-    ponerNUsers(users,loading);
+    ponerNUsers(users,loading,true);
 
-    document.getElementById("btn").addEventListener("click",function() {
-        console.log("hola");
-    });
+    
     
 });
