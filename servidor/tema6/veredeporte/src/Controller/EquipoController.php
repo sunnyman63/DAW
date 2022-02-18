732ec280f7b57f22630a8ebde13f89d1802fcde8
Alejandro Romero Pino <alerompin2@alu.edu.gva.es>
2022-02-18 19:30:07 +0100

    PHP 18/02/22

--- servidor/tema6/veredeporte/src/Controller/EquipoController.php
+++ servidor/tema6/veredeporte/src/Controller/EquipoController.php
@@ -14,13 +14,20 @@ class EquipoController extends AbstractController
     /**
      * @Route("/equipo", name="app_equipo")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $soliEquipo = "";
         $this->denyAccessUnlessGranted('ROLE_USER');
-
+        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+        if($usu[0]->getEquipo()) {
+            $equipo = $usu[0]->getEquipo();
+            $soliEquipo = $equipo->getSolicitudes();
+        }
         return $this->render('equipo/equipo.html.twig', [
             'controller_name' => 'EquipoController',
             'user' => $this->getUser(),
+            'solicitudes' => $usu[0]->getSolicitudes(),
+            'soliEquipo' => $soliEquipo,
         ]);
     }
 
@@ -28,22 +35,52 @@ class EquipoController extends AbstractController
      * @Route("/equipo/buscar", name="app_buscar_equipo")
      */
     public function listarEquipos(EntityManagerInterface $em): Response {
+        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
         $equipos = $em->getRepository(Equipo::class)->findAll();
         return $this->render('equipo/listaEquipos.html.twig', [
             'controller_name' => 'EquipoController',
             'user' => $this->getUser(),
             'equipos' => $equipos,
+            'solicitudes'=>$usu[0]->getSolicitudes(),
         ]);
     }
 
     /**
      * @Route("/equipo/solicitar/{id}", name="app_solicitar_unirse")
      */
-    public function solicitarUnirse($id): Response {
-        return $this->render('equipo/listaEquipos.html.twig', [
+    public function solicitarUnirse(EntityManagerInterface $em, $id): Response {
+        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+        $equipo = $em->getRepository(Equipo::class)->find($id);
+        $usu[0]->addSolicitude($equipo);
+        $em->persist($usu[0]);
+        $em->persist($equipo);
+        $em->flush();
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/cancelar/{id}", name="app_cancelar_solicitud")
+     */
+    public function cancelarSolicitud(EntityManagerInterface $em, $id): Response {
+        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+        $equipo = $em->getRepository(Equipo::class)->find($id);
+        $usu[0]->removeSolicitude($equipo);
+        $em->persist($usu[0]);
+        $em->persist($equipo);
+        $em->flush();
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/prueba", name="app_prueba")
+     */
+    public function prueba(EntityManagerInterface $em): Response {
+        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+        return $this->render('pruebas.html.twig', [
             'controller_name' => 'EquipoController',
             'user' => $this->getUser(),
-            'dato' => $id,
+            'equipos' => '',
+            'solicitudes' => $usu[0]->getSolicitudes(),
         ]);
     }
 }
