b4af1ca1aa154cc88e5b220b6e843eb824229bb5
Alejandro Romero Pino <alerompin2@alu.edu.gva.es>
2022-02-20 21:32:00 +0100

    PHP 20/02/22

--- servidor/tema6/veredeporte/src/Controller/EquipoController.php
+++ servidor/tema6/veredeporte/src/Controller/EquipoController.php
@@ -12,22 +12,37 @@ use Doctrine\ORM\EntityManagerInterface;
 class EquipoController extends AbstractController
 {
     /**
-     * @Route("/equipo", name="app_equipo")
+     * @Route("/equipo/", name="app_equipo")
      */
     public function index(EntityManagerInterface $em): Response
     {
-        $soliEquipo = "";
         $this->denyAccessUnlessGranted('ROLE_USER');
-        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
-        if($usu[0]->getEquipo()) {
-            $equipo = $usu[0]->getEquipo();
-            $soliEquipo = $equipo->getSolicitudes();
+        $user = $this->getUser();
+        $soliEquipo = "";
+        $solicitudes = "";
+        $error = "";
+
+        if(!empty($error)) {
+            $error = "Error del servidor";
         }
+
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$user->getUserIdentifier()));
+            if($usu[0]->getEquipo()) {
+                $equipo = $usu[0]->getEquipo();
+                $soliEquipo = $equipo->getSolicitudes();
+            }
+            $solicitudes = $usu[0]->getSolicitudes();
+        } catch (\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
         return $this->render('equipo/equipo.html.twig', [
             'controller_name' => 'EquipoController',
-            'user' => $this->getUser(),
-            'solicitudes' => $usu[0]->getSolicitudes(),
+            'user' => $user,
+            'solicitudes' => $solicitudes,
             'soliEquipo' => $soliEquipo,
+            'error' => $error,
         ]);
     }
 
@@ -35,13 +50,24 @@ class EquipoController extends AbstractController
      * @Route("/equipo/buscar", name="app_buscar_equipo")
      */
     public function listarEquipos(EntityManagerInterface $em): Response {
-        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
-        $equipos = $em->getRepository(Equipo::class)->findAll();
+        $user = $this->getUser();
+        $equipos = "";
+        $solicitudes = "";
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$user->getUserIdentifier()));
+            $equipos = $em->getRepository(Equipo::class)->findAll();
+            $solicitudes = $usu[0]->getSolicitudes();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
         return $this->render('equipo/listaEquipos.html.twig', [
             'controller_name' => 'EquipoController',
-            'user' => $this->getUser(),
+            'user' => $user,
             'equipos' => $equipos,
-            'solicitudes'=>$usu[0]->getSolicitudes(),
+            'solicitudes'=> $solicitudes,
+            'error' => $error,
         ]);
     }
 
@@ -49,12 +75,18 @@ class EquipoController extends AbstractController
      * @Route("/equipo/solicitar/{id}", name="app_solicitar_unirse")
      */
     public function solicitarUnirse(EntityManagerInterface $em, $id): Response {
-        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
-        $equipo = $em->getRepository(Equipo::class)->find($id);
-        $usu[0]->addSolicitude($equipo);
-        $em->persist($usu[0]);
-        $em->persist($equipo);
-        $em->flush();
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $equipo = $em->getRepository(Equipo::class)->find($id);
+            $usu[0]->addSolicitude($equipo);
+            $em->persist($usu[0]);
+            $em->persist($equipo);
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
         return $this->redirectToRoute('app_equipo');
     }
 
@@ -62,12 +94,110 @@ class EquipoController extends AbstractController
      * @Route("/equipo/cancelar/{id}", name="app_cancelar_solicitud")
      */
     public function cancelarSolicitud(EntityManagerInterface $em, $id): Response {
-        $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
-        $equipo = $em->getRepository(Equipo::class)->find($id);
-        $usu[0]->removeSolicitude($equipo);
-        $em->persist($usu[0]);
-        $em->persist($equipo);
-        $em->flush();
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $equipo = $em->getRepository(Equipo::class)->find($id);
+            $usu[0]->removeSolicitude($equipo);
+            $em->persist($usu[0]);
+            $em->persist($equipo);
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/denegar/{id}", name="app_denegar_solicitud")
+     */
+    public function denegarSolicitud(EntityManagerInterface $em, $id): Response {
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $usuRechazado = $em->getRepository(Usuario::class)->find($id);
+            $equipo = $em->getRepository(Equipo::class)->find($usu[0]->getEquipo()->getId());
+            $usuRechazado->removeSolicitude($equipo);
+            $em->persist($usuRechazado);
+            $em->persist($equipo);
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/aceptar/{id}", name="app_aceptar_solicitud")
+     */
+    public function aceptarSolicitud(EntityManagerInterface $em, $id): Response {
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $usuAceptado = $em->getRepository(Usuario::class)->find($id);
+            $equipo = $em->getRepository(Equipo::class)->find($usu[0]->getEquipo()->getId());
+            $usuAceptado->removeSolicitude($equipo);
+            $equipo->addJugadore($usuAceptado);
+            $em->persist($usuAceptado);
+            $em->persist($equipo);
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/expulsar/{id}", name="app_expulsar_jugador")
+     */
+    public function expulsarJugador(EntityManagerInterface $em, $id): Response {
+        $error = "";
+        try {
+            $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $usuExpulsado = $em->getRepository(Usuario::class)->find($id);
+            $equipo = $em->getRepository(Equipo::class)->find($usu[0]->getEquipo()->getId());
+            $equipo->removeJugadore($usuExpulsado);
+            $em->persist($usuExpulsado);
+            $em->persist($equipo);
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
+        return $this->redirectToRoute('app_equipo');
+    }
+
+    /**
+     * @Route("/equipo/salir/", name="app_dejar_equipo")
+     */
+    public function dejarEquipo(EntityManagerInterface $em): Response {
+        $error = "";
+        try {
+
+            $usu = $em->getRepository(Usuario::class)->findOneBy(array('email'=>$this->getUser()->getUserIdentifier()));
+            $equipo = $em->getRepository(Equipo::class)->find($usu->getEquipo()->getId());
+            if($this->isGranted("ROLE_CAPITAN")) {
+                $jugadores = $em->getRepository(Usuario::class)->findBy(array("equipo" => $equipo->getId()));
+                foreach($jugadores as $jugador) {
+                    $equipo->removeJugadore($jugador);
+                    $em->persist($jugador);
+                }
+                $usu->setRoles(["ROLE_USER"]);
+                $em->remove($equipo);
+            } else {
+                $equipo->removeJugadore($usu);
+                $em->persist($usu);
+                $em->persist($equipo);
+            }
+            
+            $em->flush();
+        } catch(\Exception $e) {
+            $error = "Error del servidor";
+        }
+        
         return $this->redirectToRoute('app_equipo');
     }
 
@@ -76,11 +206,18 @@ class EquipoController extends AbstractController
      */
     public function prueba(EntityManagerInterface $em): Response {
         $usu = $em->getRepository(Usuario::class)->findBy(array('email'=>$this->getUser()->getUserIdentifier()));
+        $equipo = $em->getRepository(Equipo::class)->find($usu[0]->getEquipo()->getId());
+        $jugadores = $em->getRepository(Usuario::class)->findBy(array("equipo" => $equipo->getId()));
+        if(!empty($jugadores)) {
+            $jugador = $jugadores;
+        }
         return $this->render('pruebas.html.twig', [
             'controller_name' => 'EquipoController',
             'user' => $this->getUser(),
             'equipos' => '',
             'solicitudes' => $usu[0]->getSolicitudes(),
+            'jugadores' => $jugadores,
+            'jugador' => $jugador,
         ]);
     }
 }
