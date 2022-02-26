4cbce39ceab5f995ca590152b7962ddd93a5cbc5
Alejandro Romero Pino <alerompin2@alu.edu.gva.es>
2022-02-26 19:46:43 +0100

    CSS 26/02/22

--- disenyo/bomberoOpositor/js/main.js
+++ disenyo/bomberoOpositor/js/main.js
@@ -8,6 +8,7 @@ var bombero = {
         let tl = gsap.timeline();
         switch(direccion) {
             case "Izquierda":
+                bombero.css("background-image","url('./img/bombero2.png')");
                 if(left>0) {
                     if(left>=20) {
                         tl.to(bombero, {duration: 0.1, x:"-=20"});
@@ -17,6 +18,7 @@ var bombero = {
                 }
                 break;
             case "Derecha":
+                bombero.css("background-image","url('./img/bombero.png')");
                 if(left<900) {
                     if(left<=880) {
                         tl.to(bombero, {duration: 0.1, x:"+=20"});
@@ -26,6 +28,27 @@ var bombero = {
                 }
                 break;
         }
+    },
+
+    disparar:function(edificio) {
+
+        let oldChorro = $(".chorro");
+        if(oldChorro.length > 0) {
+            oldChorro.remove();
+        }
+
+        let posBombero = $("#bombero").position();
+        let chorro = '<div class=\"chorro\" style=\"left:'+ posBombero.left +'px;\">'
+                            +'<img src=\"./img/agua.png\">'
+                     +'</div>';
+        
+        edificio.append(chorro);
+        let newtl = gsap.timeline({onComplete: function() {
+            $(".chorro").remove();
+        }});
+
+        newtl.to($(".chorro"), {duration: 0.3, ease: "none", y:"-=630"});
+
     }
 }
 
@@ -45,7 +68,7 @@ var persona = {
 
     crearPersona:function(edificio) {
         let aleatorio = Math.floor(Math.random() * (10 - 0)) + 0;
-        let persona = '<div class=\"persona persona' + aleatorio + '\">'
+        let persona = '<div class=\"objeto objeto' + aleatorio + '\" data-info="persona">'
                             +'<img src=\"' + this.imagenes[aleatorio] + '\">'
                      +'</div>';
         edificio.append(persona);
@@ -53,53 +76,114 @@ var persona = {
     }
 }
 
+var fuego = {
+    imagen: './img/fuego.png',
+
+    crearFuego:function(edificio) {
+        let aleatorio = Math.floor(Math.random() * (10 - 0)) + 0;
+        let fuego = '<div class=\"objeto objeto' + aleatorio + '\" data-info="fuego">'
+                            +'<img src=\"' + this.imagen + '\">'
+                     +'</div>';
+        edificio.append(fuego);
+        return aleatorio;
+    }
+}
+
 var intervalo = 0;
 
 function caer(objeto) {
-    let persona = $(objeto);
-    let position = persona.offset();
+    let cuerpo = $(objeto);
+    let position = cuerpo.position();
     let top = position.top;
     let tl = gsap.timeline({onComplete: function() {
-        persona.remove();
         clearInterval(intervalo);
-        let vidas = parseInt($("#numVidas").text())-1;
-        if(vidas > 0) {
-            $("#numVidas").text(vidas);
-            console.log("muelto");
-            juego();
+        console.log("muelto");
+        cuerpo.remove();
+        if(cuerpo.data("info") == "persona") {
+            let vidas = parseInt($("#numVidas").text())-1;
+            if(vidas > 0) {
+                $("#numVidas").text(vidas);
+                juego();
+            } else {
+                $("#numVidas").text(vidas);
+                final();
+            }
         } else {
-            let final ='<div class="menu">' 
-                        + '<div class="pautas"><div>Has perdido!!</div></div>'
-                        + '<div class="boton"><a href=".">JUGAR OTRA VEZ</a></div>'
-                     + '</div>'
-            $(".menu").remove();
-            $(".inicio").append(final);
-            $(".inicio").fadeIn();
+            let pts = parseInt($("#pts").text());
+            pts = pts - 5;
+            $("#pts").text(pts);
+            juego();
         }
+        
     }});
     if(top == 140) {
-        tl.to(persona, {duration: 4, ease: "none", y:"+=535"});
-        intervalo = setInterval(function(){
-            if(colision(persona,$("#bombero"))) {
-                tl.pause();
-                persona.remove();
-                clearInterval(intervalo);
-                console.log("vivo");
-                juego();
-            }
-        },24);
+        tl.to(cuerpo, {duration: 3, ease: "none", y:"+=540"});
     } else {
-        tl.to(persona, {duration: 3, ease: "none", y:"+=285"});
-        intervalo = setInterval(function(){
-            if(colision(persona,$("#bombero"))) {
-                tl.pause();
-                persona.remove();
-                clearInterval(intervalo);
+        tl.to(cuerpo, {duration: 2, ease: "none", y:"+=285"});
+    }
+
+    intervalo = setInterval(function(){
+        if(colision(cuerpo,$("#bombero"))) {
+            clearInterval(intervalo);
+            let pts = parseInt($("#pts").text());
+            if(cuerpo.data("info") == "persona") {
                 console.log("vivo");
+                pts = pts + 10;
+                $("#pts").text(pts);
+            } else {
+                pts = pts - 10;
+                $("#pts").text(pts);
+            }
+            tl.pause();
+            cuerpo.remove();
+            
+            if(pts >= 200) {
+                final(pts);
+            } else {
                 juego();
             }
-        },24);
+        }
+        
+        if(cuerpo.data("info") == "fuego") {
+            let chorro = $(".chorro");
+            if(chorro.length > 0) {
+                if(colision(cuerpo,$(".chorro"))) {
+                    clearInterval(intervalo);
+                    let pts = parseInt($("#pts").text());
+                    console.log("apagado");
+                    tl.pause();
+                    cuerpo.remove();
+                    $(".chorro").remove();
+                    pts = pts + 10;
+                    $("#pts").text(pts);
+    
+                    if(pts >= 200) {
+                        final(pts);
+                    } else {
+                        juego();
+                    }
+                }
+            }       
+        }
+        
+    },24);
+}
+
+function final(pts) {
+    let msg = "";
+    console.log(pts);
+    if(pts >= 200) {
+        msg = "Has ganadoooooooooooooo!!!";
+    } else {
+        msg = "Has perdido! :("
     }
+    let final ='<div class="menu">' 
+    + '<div class="pautas"><div>'+msg+'</div></div>'
+    + '<div class="boton"><a href=".">JUGAR OTRA VEZ</a></div>'
+    + '</div>'
+    $(".menu").remove();
+    $(".inicio").append(final);
+    $(".inicio").fadeIn();
 }
 
 function colision(objeto,bombero) {
@@ -107,7 +191,6 @@ function colision(objeto,bombero) {
     let posBombero = bombero.position();
     if(posObjeto.top >= (posBombero.top - 100)) {
         if(posObjeto.left >= (posBombero.left - 150) && posObjeto.left < (posBombero.left + 50)) {
-            //console.log("posObj -> "+ posObjeto.left + ", posBomb -> " + posBombero.left);
             return true;
         } else {
             return false;
@@ -118,24 +201,35 @@ function colision(objeto,bombero) {
 }
 
 function juego() {
-    let person = ".persona" + persona.crearPersona($("#edificio"));
-    caer(person);
+    let aleatorio = Math.floor(Math.random() * (10 - 0)) + 0;
+    let objeto = ".objeto";
+    if(aleatorio <= 5) {
+        objeto = objeto + persona.crearPersona($("#edificio"));
+    } else {
+        objeto = objeto + fuego.crearFuego($("#edificio"));
+    }
+    
+    caer(objeto);
 }
 
-$(document).on('keypress', function(e) {
+$(document).on('keydown', function(e) {
     var key = e.keyCode;  
     switch(key) {
-        case 97:
+        case 37:
             bombero.mover("Izquierda");
             break;
-        case 100:
+        case 39:
             bombero.mover("Derecha");
+            break;
+        case 38:
+            bombero.disparar($('#edificio'))
             break;
     }
 });
 
 $("#boton").click(function (e) { 
     $(".inicio").fadeOut();
+    $(".bombero").css('bottom','0px');
     juego();
 });
 
